de421fdc19b89cf6a0f9ce7915497792d22df6cd
silva2852 <167568800+silva2852@users.noreply.github.com>
2026-08-24 11:38:12 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -188,8 +188,8 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-const PORT = process.env.PORT
-|| 3000:
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`porta $(PORT)`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
