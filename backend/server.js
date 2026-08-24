cf7bcb22e90fcd8a58e5501fd9198f78a5d9ece2
silva2852 <167568800+silva2852@users.noreply.github.com>
2026-08-24 11:19:23 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
